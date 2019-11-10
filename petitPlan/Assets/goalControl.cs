0f101f4f99809c5b5149ab86dd8871c3196f9d2c
3DPIT <oksos0817@naver.com>
2019-11-10 17:25:13 +0900

    gameFinish

---
+++ petitPlan/Assets/goalControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goalControl : MonoBehaviour
+{
+    private bool is_collided = false;// 충돌했는지 안했는지 알려주는거 충돌시 true 이므로 초기화 false
+    public float GOAL_MIN = 5.0f;
+    public float GOAL_MAX = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float rnd = Random.Range(GOAL_MIN, GOAL_MAX);//랜덤으로 x 위치 받아서
+        this.transform.position = new Vector3(rnd,-1.0f, 0.0f);//x 축에 저장해서 실행
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    private void OnCollisionStay(Collision other)// 다른 게임 오브젝트와 충돌하는 동안 계속 호출하는 것
+    {
+        this.is_collided = true;
+
+    }
+    private void OnGUI()
+    {
+        if (is_collided)//        if (is_collided==true)와 같은 표현 참이면 들어가라라는 의미
+        {
+            GUI.Label(new Rect(Screen.width / 2, 80, 100, 20), "성공");
+        }
+    }
+}
---
+++ petitPlan/Assets/playerControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class playerControl : MonoBehaviour
+{
+    private float power;
+    public float POWERPLUS = 100.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))// 왼쪽 버튼이 눌리면
+        {
+            power += POWERPLUS * Time.deltaTime;// 힘을 모은다. 기를 모으는것
+        }
+        if (Input.GetMouseButtonUp(0))//왼족 버튼을 떼면
+        {
+            this.transform.GetComponent<Rigidbody>().AddForce(new Vector3(power, power, 0)); // 힘을 x 와 y 에 반영
+            power = 0.0f;// 파워 초기화
+        }
+        if (this.transform.position.y < -5.0f)//지면보다 아래 -5.0f 까지 떨어지면 구명에 떨어진 순간
+        {
+            Application.LoadLevel("gameScene");//씬을 다시 로드 다시 시작의 개념
+        }
+    }
+}
